445e12048f77fc61f69b2994f5eb0c7c2b6c7de2
asm_yx <blues2600@qq.com>
2022-06-06 16:19:29 +0000

    update

---
+++ binary_search.asm
@@ -0,0 +1,95 @@
+include		environment.inc		; 编程环境声明
+
+.data
+.code
+
+; 名称：	binary_search
+; 功能：	查询中断向量表地址
+; 参数： pPage_table:ptr dword
+;		query_value:dword
+;		number_size:dword
+;		table_size:dword
+; 要求： 表的内容按照升序排列
+; 实现： 使用对半查找将query_value与表格中的值对比
+; 返回：	如果成功则EAX = 中断处理程序指针，否则返回0
+
+binary_search proc
+					pPage_table		equ		[ebp+8]			;表指针
+					query_value		equ		[ebp+12]		;查询值，中断信号
+					member_size		equ		[ebp+16]		;表项大小
+					table_size		equ		[ebp+20]		;表的元素个数
+
+					first			equ		[ebp-4]			;第一个元素下标
+					last			equ		[ebp-8]			;最后一个元素下标
+					mid				equ		[ebp-12]		;中间元素下标
+
+					push			ebp
+					mov				ebp,esp
+					sub				esp,12
+					push			ebx
+					push			edx
+					push			edi
+
+					;初始化
+					mov  eax,0
+					mov	 first,eax				; first = 0
+					mov	 eax,table_size			; last = (table_size - 1)
+					dec	 eax
+					mov	 last,eax
+					mov	 edi,query_value		; EDI = searchVal
+					mov	 ebx,pPage_table		; EBX = 表指针
+
+				start: ; while first <= last
+					mov	 eax,first
+					cmp	 eax,last
+					ja	 err					; 退出查找
+
+					; mid = (last + first) / 2
+					mov	 eax,last
+					add	 eax,first
+					shr	 eax,1
+					mov	 mid,eax
+
+					; EDX = 中间元素的值
+					mov	 edx,0
+					mul	 dword ptr member_size				; eax * mid
+					mov	 edx,[ebx+eax]				; EDX = 中间元素的值
+
+					; if ( EDX < query_value(EDI) )
+					;	first = mid + 1;
+					cmp	 edx,edi
+					jae	 L2
+					mov	 eax,mid				; first = mid + 1
+					add	 eax,1
+					mov	 first,eax
+					jmp	 loop_point
+
+					; else if( EDX > query_value(EDI) )
+					;	last = mid - 1;
+				L2:	cmp	 edx,edi
+					jbe	 L3
+					mov	 eax,mid				; last = mid - 1
+					sub	 eax,1
+					mov	 last,eax
+					jmp	 loop_point
+
+					; else return mid
+				L3:	mov	 eax,mid  				; 找到要查询的值，中断信号
+					mov  edx,0					; 下标 * 元素大小
+					mul  dword ptr member_size
+					add  eax,ebx				; 元素地址
+					add  eax,4					; 返回中断处理程序地址
+					jmp	 return						; return (mid)
+
+		loop_point:	jmp	 start
+				
+				err:mov			eax,0
+				return:
+					pop			edi
+					pop			edx
+					pop			ebx
+					mov			esp,ebp
+					pop			ebp
+					ret			16
+binary_search endp
+end
---
+++ yx_interrupt_handling.asm
@@ -0,0 +1,104 @@
+
+
+;********************************************************************
+;
+;				名称：yx_interrupt_handling
+;				功能：CPU中断处理程序
+;				说明：不考虑硬件，使用纯软件的方式构建中断，本程序用于理解cpu中断机制
+;				创建时间：2022年5月28日
+;				作者：yx
+;
+;********************************************************************
+
+include		environment.inc
+include		Irvine32.inc
+include		win32.inc
+extern binary_search@0:proc								;对半查找过程
+binary_search equ binary_search@0
+
+.data 
+signal						dword		0				;中断信号
+fpu_mmx_register			byte		512		dup(0)	;保存fpu mmx寄存器组
+interrupt_vector_table		dword		0,123h			;中断向量表
+member_size	= ($ - interrupt_vector_table)				;中断向量表项大小
+							dword		11,123h
+							dword		21,1ABh
+							dword		31,123h
+							dword		41,123h
+							dword		51,123h
+							dword		61,123h
+
+.code
+
+
+;		**** 技术说明 ****
+;	
+;	写在前面：由于中断处理需要硬件介入，所以在本程序中存在许多假设
+
+;	中断发生时上下文的保存：
+;		在检测到中断后，假设自动cpu保存当前eip的值，并且在中断返回时它能够自动恢复eip的值
+;		保存其他上下文的工作则由中断处理程序来处理
+
+;	cpu的中断处理过程：
+;		cpu检测到中断存在 - 保存当前eip - 将eip指向中断处理程序
+
+;	中断处理程序的工作过程：
+;		保存当前上下文 - 根据signal查询中断向量表 - 根据查询结果调用中断服务程序 - 中断服务程序执行完成 - 恢复上下文 - cpu恢复中断前的eip
+;
+;	机器平台：x86 32bit
+;	中断信号：引发中断的设备ID、预定义的中断信号
+;	中断信号的存储和传递：
+;		cpu检测到中断后，中断处理程序必须根据中断信号来选择中断服务程序
+;		在本程序中，约定在变量signal中存储中断信号。中断处理程序默认从signal读取中断信号的值。
+;		当一个进程或IO设备请求中断，它通过修改signal的值来完成。
+					
+;	中断处理程序本身的几个问题：
+;		从源代码的角度看中断处理程序是一个过程，但从CPU运行的角度，中断处理程序不会具备完整的过程特征，
+;		甚至不被当做一个过程来执行。首先，它不会有参数（无法传递），此外中断处理程序没有返回，也就是说它实际上仅是一段二进制代码。
+;		
+;	这个程序的缺失部分：
+;		cpu必须具备当检测到中断时自动保存当前eip的能力
+;		cpu必须事先了解中断处理程序的地址
+;		当中断处理程序完成后，要么由中断处理程序恢复中断前的eip，要么由cpu自己恢复
+;		以上部分都依赖硬件的支持和配合
+;		有一些控制寄存器和调试寄存器没有纳入其中，因为构建本程序的主要目的在于理解中断机制
+;		加上自己希望尝试构建一个中断处理程序，所以忽略了其他部分的细节
+
+main PROC
+
+	;保存上下文，忽略了部分寄存器组
+	pushad							;通用寄存器组
+	pushfd							;eflags
+;	fxsave		fpu_mmx_register	;用户模式无法通过汇编
+	push		gs
+	push		fs
+	push		es
+	push		ss
+	push		ds
+	;push		cs
+
+	;查询中断向量表
+	push		7								;表的元素个数
+	push		8								;表项大小
+	push		21								;查询的值
+	push		offset interrupt_vector_table	;中断向量表指针
+	call		binary_search
+
+	; 此时eax = 中断服务程序地址
+	; 执行中断服务程序，需要硬件介入
+
+	;恢复上下文
+;	pop			cs				;段寄存器的修改无法通过汇编
+	pop			ds
+	pop			ss
+	pop			es
+	pop			fs
+	pop			gs
+;	fxrstor	fpu_mmx_register	;用户模式无法通过汇编
+	popfd
+	popad
+
+		
+	invoke		ExitProcess,0
+main ENDP
+END main
