6cba24073d1b7f372a542719b690d94e18283c32
asm_yx <blues2600@qq.com>
2022-07-20 07:45:58 +0000

    update binary_search.asm.

--- binary_search.asm
+++ binary_search.asm
@@ -97,21 +97,21 @@ end
 ; binary search Java 实现
 class Solution {
     public int search(int[] nums, int target) {
-;        int left = 0, right = nums.length - 1;
-;        while (left <= right) {
-;            int mid = (right - left) / 2 + left;
-;            int num = nums[mid];
-;            if (num == target) {
-;                return mid;
-;            } else if (num > target) {
-;                right = mid - 1;
-;            } else {
-;                left = mid + 1;
-;            }
-;        }
-;        return -1;
-;    }
-;}
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
 
 ;作者：LeetCode-Solution
 ;链接：https://leetcode.cn/problems/binary-search/solution/er-fen-cha-zhao-by-leetcode-solution-f0xw/
