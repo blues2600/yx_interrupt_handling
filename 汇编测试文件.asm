1d927b4c3e5a410593f1282a576f0bed97895a44
asm_yx <blues2600@qq.com>
2022-07-22 03:33:37 +0000

    update 汇编测试文件.asm.

--- 汇编测试文件.asm
+++ 汇编测试文件.asm
@@ -11,4 +11,13 @@ mov  eax , 0110101110b      ;二进制
 
 mov  eax , 1  	            ;将数值1移动到eax寄存器
 mov  eax , ebx              ;将ebx的值移动（复制）到eax
-mov  eax , [0x00400000]     ;将内存地址 0x00400000 的数据移动（复制）到eax
+
+;将内存地址 0x00400000 的数据移动（复制）到eax
+;dword ptr 意味着地址0x00400000的数据是一个dword
+mov  eax , dword ptr [0x00400000]     
+
+mov  eax , 400000h          ;将数值0x00400000移动到eax
+
+;将eax的值视为一个内存地址，此时eax是一个指针
+;将ecx的值移动到ecx指向的位置，即0x00400000位置
+mov  dword ptr [eax] , ecx
