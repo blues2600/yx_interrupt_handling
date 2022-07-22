33137339b17894492f8e20f22bdc81867c85302e
asm_yx <blues2600@qq.com>
2022-07-22 03:28:41 +0000

    update 汇编测试文件.asm.

--- 汇编测试文件.asm
+++ 汇编测试文件.asm
@@ -5,3 +5,10 @@ mov  eax , 0x10             ;十六进制
 mov  eax , 10h              ;十六进制
 mov  eax, 0A12Bh            ;十六进制
 mov  eax , 0110101110b      ;二进制
+
+
+
+
+mov  eax , 1  	            ;将数值1移动到eax寄存器
+mov  eax , ebx              ;将ebx的值移动（复制）到eax
+mov  eax , [0x00400000]     ;将内存地址 0x00400000 的数据移动（复制）到eax
